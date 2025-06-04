23296868b84683d8ea4b6f7e8ef8529dfd442256
Nihar Patel <niharpatel52@gmail.com>
2025-06-04 07:15:57 -0400

    create config.js and .gitignore for API

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const apiKey = "d0vf12pr01qkepcvrurgd0vf12pr01qkepcvrus0";
-// const ticker = "AAPL";
-// const from = "2025-05-01";
-// const to = "2025-06-03";
-
-// fetch(`https://finnhub.io/api/v1/company-news?symbol=${ticker}&from=${from}&to=${to}&token=${apiKey}`)
-//   .then(res => res.json())
-//   .then(data => displayNews(data))
-//   .catch(err => console.error(err));
-
 // Create a news section
 const newsContainer = document.getElementById('news-articles');
 
@@ -29,12 +19,14 @@ const displayNews = (articles) => {
 
 //Grab Ticker from Stock-Search
 const searchInput = document.getElementById("stock-search");
+searchInput.innerHTML = "";
 
 const handleTickerChange = (event) => {
   if (event.key === "Enter") {
   const newTicker = event.target.value.toUpperCase();
   console.log(newTicker);
-  fetchNews(newTicker)
+  fetchNews(newTicker);
+  renderSymbolInfo(newTicker);
   }
 };
 
@@ -43,13 +35,59 @@ searchInput.addEventListener("keydown", handleTickerChange);
 //Create Fetch News Function to fetch when a new stock is enetered
 
 const fetchNews = (ticker) => {
+  console.log("Fetching news for:", ticker);
   const from = "2025-05-01"
   const to = "2025-06-03"
   const url = `https://finnhub.io/api/v1/company-news?symbol=${ticker}&from=${from}&to=${to}&token=${apiKey}`;
-  console.log(ticker);
+   console.log("URL:", url);
 
   fetch (url) 
   .then(res => res.json())
   .then(data => displayNews(data))
   .catch(err => console.error(err));
 }
+
+//Create inline JS forTradingView WIdgets to dynimically update with ticker change
+
+// <!-- TradingView Widget BEGIN -->
+// <div class="tradingview-widget-container">
+//   <div class="tradingview-widget-container__widget"></div>
+//   <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank"><span class="blue-text">Track all markets on TradingView</span></a></div>
+//   <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-symbol-info.js" async>
+//   {
+//   "symbol": "NASDAQ:AAPL",
+//   "width": 550,
+//   "locale": "en",
+//   "colorTheme": "dark",
+//   "isTransparent": false
+// }
+//   </script>
+// </div>
+// <!-- TradingView Widget END -->
+
+const renderSymbolInfo = (ticker) => {
+  const container = document.getElementById("symbol-info");
+  console.log(1);
+  container.innerHTML = "";
+ console.log(2);
+  const script = document.createElement("script");
+   console.log(3);
+  script.src = "https://s3.tradingview.com/external-embedding/embed-widget-symbol-info.js";
+   console.log(4);
+  script.type = "text/javascript"
+   console.log(5);
+  script.async = true;
+ console.log(6);
+
+  script.innerHTML = JSON.stringify({
+    symbol: `NASDAQ:${ticker}`,
+    width: "100%",
+    locale: "en",
+    colorTheme: "light",
+    isTransparent: false,
+  })
+   console.log(7);
+  container.appendChild(script);
+  console.log(8);
+
+};
