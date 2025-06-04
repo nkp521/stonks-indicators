4cd1f31a00a4f87132e002aa50596442f00ad54e
Nihar Patel <niharpatel52@gmail.com>
2025-06-04 07:33:47 -0400

    create renderAdvancedChart to dynamically append Symbol Info

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const handleTickerChange = (event) => {
   console.log(newTicker);
   fetchNews(newTicker);
   renderSymbolInfo(newTicker);
+  renderAdvancedChart(newTicker);
   }
 };
 
@@ -69,3 +70,50 @@ const renderSymbolInfo = (ticker) => {
 
 fetchNews("AAPL");
 renderSymbolInfo("AAPL");
+
+    //   <!-- TradingView Advanced Chart Widget BEGIN -->
+    //   <div class="tradingview-widget-container">
+    //     <div class="tradingview-widget-container__widget"></div>
+    //     <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js" async>
+    //     {
+    //       "width": "980",
+    //       "height": "100%",
+    //       "symbol": "NASDAQ:AAPL",
+    //       "interval": "60",
+    //       "timezone": "America/New_York",
+    //       "theme": "light",
+    //       "style": "1",
+    //       "locale": "en",
+    //       "hide_top_toolbar": true,
+    //       "allow_symbol_change": true,
+    //       "studies": ["STD;RSI"],
+    //       "support_host": "https://www.tradingview.com"
+    //     }
+    //     </script>
+    //   </div>
+    //   <!-- TradingView Advanced Chart Widget END -->
+    // </section>
+
+    const renderAdvancedChart = (ticker) => {
+      const container = document.getElementById("advanced-chart");
+      container.innerHTML = "";
+
+      const script = document.createElement("script");
+      script.src = "https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js";
+      script.type = "text/javascript";
+      script.async = true;
+
+      script.innerHTML = JSON.stringify({
+        autosize: true,
+        symbol: `NASDAQ:${ticker}`,
+        interval: "D",
+        timezone: "America/New_York",
+        theme: "light",
+        style: "1",
+        locale: "en",
+        allow_symbol_change: true,
+      });
+
+      container.appendChild(script);
+    };
+  renderAdvancedChart("AAPL");
